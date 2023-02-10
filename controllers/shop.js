15f53d44ccfe4e6e677763d3ee3954fc424191e8
ikun <0215.miyatto@gmail.com>
2023-02-11 07:01:29 +0900

    カートモデルとカートに商品を追加する機能の実装

--- controllers/shop.js
+++ controllers/shop.js
@@ -106,32 +106,84 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart(cart => {
-    Product.fetchAll(products => {
-      const cartProducts = [];
-      for (let product of products) {
-        const cartProductData = cart.products.find(prod => prod.id === product.id);
-        if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty });
-        }
-      }
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: cartProducts
-      });
+  req.user.getCart()
+    .then(cart => {
+      console.log(cart);
+      return cart.getProducts()
+        .then(products => {
+          res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
     })
-  });
+    .catch(err => {
+      console.log(err);
+    });
+  // Cart.getCart(cart => {
+  //   Product.fetchAll(products => {
+  //     const cartProducts = [];
+  //     for (let product of products) {
+  //       const cartProductData = cart.products.find(prod => prod.id === product.id);
+  //       if (cartProductData) {
+  //         cartProducts.push({ productData: product, qty: cartProductData.qty });
+  //       }
+  //     }
+  //     res.render('shop/cart', {
+  //       path: '/cart',
+  //       pageTitle: 'Your Cart',
+  //       products: cartProducts
+  //     });
+  //   })
+  // });
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  console.log(prodId);
-  Product.findById(prodId, product => {
-    console.log(product);
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  let fetchedCart;
+  let newQuantity = 1;
+  req.user.getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId }});
+    })
+    .then(products => {
+      let product;
+      // 既に商品がカートに入っている場合はnewQuantityを増やす
+      if (products.length > 0){
+        product = products[0];
+      }
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      // 商品がカートに入っていなかった場合は商品オブジェクトを返す
+      return Product.findByPk(prodId)
+    })
+    .then(product => {
+      // 商品がカートに入っていなかった場合の処理
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity }
+      });
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  // const prodId = req.body.productId;
+  // console.log(prodId);
+  // Product.findById(prodId, product => {
+  //   console.log(product);
+  //   Cart.addProduct(prodId, product.price);
+  // });
+  // res.redirect('/cart');
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
