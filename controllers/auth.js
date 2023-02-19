27716135f97dcdd40d0e5ea930166ea43a7e3132
ikun <0215.miyatto@gmail.com>
2023-02-20 06:08:01 +0900

    sessionの設定

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req.get('Cookie').split(';')[1].trim().split('=')[1];
   // console.log(isLoggedIn);
+  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Longin',
@@ -9,6 +10,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true;HttpOnly;');
+  req.session.isLoggedIn = true;
   res.redirect('/');
 };
